fdf5fefd7fb0d04d7b2bedfffbd293709a60a145
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-28 00:06:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+attr_reader :roster
+attr_writer :roster
+
+test = "test"
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+    @roster[grade] = []
+    @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    roster.values_at(grade).flatten
+  end
+
+  def sort(test)
+    @roster.sort
+  end
+
+end
